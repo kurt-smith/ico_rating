e202f07b3521a34dd7d6ede006547bef72bcbbd2
khaos <dev.ikhaos@gmail.com>
2018-02-06 18:34:22 -0500

    initial commit

---
+++ lib/ico_rating.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: false
+
+require 'active_support'
+require 'active_support/core_ext'
+require 'ico_rating/version'
+require 'http'
+require 'nokogiri'
+
+class IcoRating
+  BASE_URL = 'https://icorating.com'.freeze
+end
---
+++ lib/ico_rating/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class IcoRating
+  VERSION = '0.1.0'
+end
